f7d5bceb2d69d7c21a4e2ed942aad4f64b5a0461
vciaccio88 <vincenzo.ciaccio@gmail.com>
2016-05-13 10:05:22 +0200

    fix on radio view list

--- api/config/feeds.php
+++ api/config/feeds.php
@@ -4,46 +4,68 @@ return [
     'radios' => [
         [
             'name' => 'radio2',
+            'logo' => 'http://www.radio2.rai.it/dl/img/2014/07/1406711506246Radio2-Logo-new-640x360.jpg',
             'baseurl' => 'http://www.radio.rai.it/radio2/podcast/rssradio2.jsp?id=',
             'shows' => [
-                '610' => [
-                    'img' => 'http://rumors.blog.rai.it/files/2013/05/610-10.jpg',
+                [
+                    'name' => '610',
+                    'label' => '610: Lillo e Greg',
+                    'img' => 'http://wwwra.ansa.it/webimages/img_457x/2015/9/3/e3aa718a40c45f498218cadf4f78802e.jpg',
                     'code' => '80'
                 ],
-                'iprovinciali' => [
-                    'img' => '',
+                [
+                    'name' => 'iprovinciali',
+                    'label' => 'I Provinciali',
+                    'img' => 'http://www.rai.tv/dl/img/2015/05/1432043024245I-PROVINCIALI.png',
                     'code' => '13607'
                 ],
-                'ilruggitodelconiglio' => [
-                    'img' => '',
+                [
+                    'name' => 'ilruggitodelconiglio',
+                    'label' => 'Il Ruggito del Coniglio',
+                    'img' => 'http://www.radio2.rai.it/dl/img/2015/05/1432043003360IL-RUGGITO-DEL-CONIGLIO.png',
                     'code' => '150'
                 ],
-                'caterpillar' => [
-                    'img' => '',
+                [
+                    'name' => 'caterpillar',
+                    'label' => 'Caterpillar',
+                    'img' => 'http://www.rai.tv/dl/img/2015/09/1442834197542caterpillar_ok.jpg',
                     'code' => '83'
                 ],
-                'caterpillaram' => [
-                    'img' => '',
+                [
+                    'name' => 'caterpillaram',
+                    'label' => 'Caterpillar AM',
+                    'img' => 'http://www.radio2.rai.it/dl/img/2015/05/1432042983160CATERPILLAR-AM.png',
                     'code' => '5739'
                 ]
             ]
-        ], [
+        ],
+        [
             'name' => 'radio24',
+            'logo' => 'http://www.amicidipontecarrega.it/wp-content/uploads/2013/11/radio24logo.jpg',
             'baseurl' => 'http://www.radio24.ilsole24ore.com/podcast/',
             'shows' => [
-                '2024' => [
+                [
+                    'name' => '2024',
+                    'label' => '2024 con  Enrico Pagliarini',
                     'img' => 'http://www.amicidipontecarrega.it/wp-content/uploads/2013/11/radio24logo.jpg',
                     'code' => '2024.xml'
                 ],
-                'giovani-talenti' => [
+                [
+                    'name' => 'giovani-talenti',
+                    'label' => 'Giovani Talenti',
                     'img' => 'http://www.amicidipontecarrega.it/wp-content/uploads/2013/11/radio24logo.jpg',
                     'code' => 'giovani-talenti.xml'
-                ],
-                'lazanzara' => [
+                ]
+                ,
+                [
+                    'name' => 'lazanzara',
+                    'label' => 'La Zanzara',
                     'img' => 'http://www.amicidipontecarrega.it/wp-content/uploads/2013/11/radio24logo.jpg',
                     'code' => 'lazanzara.xml'
                 ],
-                'storiacce' => [
+                [
+                    'name' => 'storiacce',
+                    'label' => 'Storiacce',
                     'img' => 'http://www.amicidipontecarrega.it/wp-content/uploads/2013/11/radio24logo.jpg',
                     'code' => 'storiacce.xml'
                 ]
